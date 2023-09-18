8186ed5b1325904f44abb5b721e4bf6a05025810
Juan Luna <lunacalvojuanignacio@gmail.com>
2023-09-18 19:26:40 -0300

    Add functionality by Country,Region 18/09/2023

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { Observable, catchError, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
-
-
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
@@ -15,8 +12,20 @@ export class CountriesService {
     constructor(private http: HttpClient) { }
 
     searchCapital ( term: string ): Observable<Country[]> {
-        return this.http.get<Country[]>(`${this.apiUrl}/capital/${ term }`);
+        return this.http.get<Country[]>(`${this.apiUrl}/capital/${ term }`)
+            .pipe ( catchError( () => of ([]) ) );
     }
-    
+
+
+    searchCountry ( term: string ): Observable <Country[]> {
+        return this.http.get<Country[]>(`${this.apiUrl}/name/${ term }`)
+        .pipe ( catchError( () => of ([]) ) );
+    }
+
+    searchRegion ( region: string ): Observable <Country[]> {
+        return this.http.get<Country[]>(`${this.apiUrl}/region/${ region }`)
+        .pipe ( catchError( () => of ([]) ) );
+    }
+ 
 }
 
